a04ce6fcdaed3b52c17396c7e54d3a1b14ac1259
izmoqwy <izmoqwy@gmail.com>
2019-10-29 21:14:59 +0100

    fix(island): border shows correctly

--- LeezIsland/src/lz/izmoqwy/leezisland/listeners/BordersListener.java
+++ LeezIsland/src/lz/izmoqwy/leezisland/listeners/BordersListener.java
@@ -21,48 +21,36 @@ import lz.izmoqwy.leezisland.island.Island;
 
 public class BordersListener implements Listener {
 
-	@EventHandler(priority=EventPriority.HIGH, ignoreCancelled=true)
-	public void onTeleport(final PlayerTeleportEvent event)
-	{
-		
-		if(event.getTo().getWorld() != GridManager.getWorld())
+	@EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+	public void onTeleport(PlayerTeleportEvent event) {
+		if (event.getTo().getWorld() != GridManager.getWorld())
 			return;
-		
-		Island island = GridManager.getIslandAt(event.getTo());
-		if(island != null)
-		{
-			
-			BorderAPI.setBorder(event.getPlayer(), island);
-			
-		}
-		
-	}
-	
-	@EventHandler(priority=EventPriority.HIGH)
-	public void onConnect(final PlayerJoinEvent event)
-	{
-		
-		if(event.getPlayer().getLocation().getWorld() != GridManager.getWorld())
-			return;
-		
-		Island island = GridManager.getIslandAt(event.getPlayer().getLocation());
-		if(island != null)
-		{
-			
-			new BukkitRunnable() {
 
+		Island island = GridManager.getIslandAt(event.getTo());
+		if (island != null) {
+			new BukkitRunnable() {
 				@Override
 				public void run() {
-					
 					BorderAPI.setBorder(event.getPlayer(), island);
-					
 				}
-				
-			}.runTaskLater(LeezIsland.getInstance(), 10);
-			
+			}.runTaskLater(LeezIsland.getInstance(), 5);
 		}
-		
-		
 	}
-	
+
+	@EventHandler(priority = EventPriority.HIGH)
+	public void onConnect(PlayerJoinEvent event) {
+		if (event.getPlayer().getLocation().getWorld() != GridManager.getWorld())
+			return;
+
+		Island island = GridManager.getIslandAt(event.getPlayer().getLocation());
+		if (island != null) {
+			new BukkitRunnable() {
+				@Override
+				public void run() {
+					BorderAPI.setBorder(event.getPlayer(), island);
+				}
+			}.runTaskLater(LeezIsland.getInstance(), 10);
+		}
+	}
+
 }
