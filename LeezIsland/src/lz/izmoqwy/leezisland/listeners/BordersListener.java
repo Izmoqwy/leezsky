d8337e2a3b20cd5738ba2b9bf5c5fa6c86023b3b
izmoqwy <izmoqwy@gmail.com>
2019-10-29 21:33:36 +0100

    fix(island): outside login

--- LeezIsland/src/lz/izmoqwy/leezisland/listeners/BordersListener.java
+++ LeezIsland/src/lz/izmoqwy/leezisland/listeners/BordersListener.java
@@ -7,17 +7,20 @@
 
 package lz.izmoqwy.leezisland.listeners;
 
+import lz.izmoqwy.core.helpers.PluginHelper;
+import lz.izmoqwy.leezisland.BorderAPI;
+import lz.izmoqwy.leezisland.LeezIsland;
+import lz.izmoqwy.leezisland.grid.GridManager;
+import lz.izmoqwy.leezisland.island.Island;
+import lz.izmoqwy.leezisland.players.SkyblockPlayer;
+import lz.izmoqwy.leezisland.players.Wrapper;
+import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerTeleportEvent;
 import org.bukkit.scheduler.BukkitRunnable;
-
-import lz.izmoqwy.leezisland.BorderAPI;
-import lz.izmoqwy.leezisland.LeezIsland;
-import lz.izmoqwy.leezisland.grid.GridManager;
-import lz.izmoqwy.leezisland.island.Island;
 
 public class BordersListener implements Listener {
 
@@ -50,6 +53,19 @@ public class BordersListener implements Listener {
 					BorderAPI.setBorder(event.getPlayer(), island);
 				}
 			}.runTaskLater(LeezIsland.getInstance(), 10);
+		}
+		else {
+			SkyblockPlayer skyblockPlayer = Wrapper.wrapPlayer(event.getPlayer());
+			if (skyblockPlayer != null) {
+				if (skyblockPlayer.hasIsland()) {
+					if (skyblockPlayer.hasIsland()) {
+						skyblockPlayer.bukkit().teleport(skyblockPlayer.getIsland().getHome());
+					}
+					else
+						PluginHelper.performCommand("spawn " + skyblockPlayer.bukkit().getName());
+				}
+				skyblockPlayer.sendMessage(ChatColor.RED + "Vous êtiez dans un endroit inconnu lors de votre reconnexion, vous avez été téléporté ailleur.");
+			}
 		}
 	}
 
